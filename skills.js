875438317c6ede67290273a123fa619f40853f77
Pramod <134430452+rajPramodArya@users.noreply.github.com>
2024-12-23 14:30:30 +0000

    added multiply function

--- skills.js
+++ skills.js
@@ -2,3 +2,8 @@ function calculateNumbers(var1, var2) {
   console.log("Pramod's first js copilot")  
   console.log(var1 + var2);
 }
+
+function multipleNumbers(var1, var2) {
+  console.log("Pramod's first js copilot")  
+  console.log(var1 * var2);
+}
